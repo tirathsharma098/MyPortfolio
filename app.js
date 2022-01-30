38c4164efaace78b932032faae2205f509f97783
Tirath Sharma <70467735+tirathsharma098@users.noreply.github.com>
2022-01-30 18:20:07 +0530

    Added some background effects and transition
    Changing shapes of border every 2s

--- app.js
+++ app.js
@@ -12,3 +12,26 @@ socialIcon.forEach( icon =>
         icon.style.border = "2px solid #ffb5a7";
     })    
 );
+
+// Here is code to make heading's title radius change every time
+
+const shapes = document.querySelectorAll(".shapeConvert");
+
+changeShape();
+
+function changeShape(){
+    let firstNum = Math.floor( Math.random()*150 ) + 50;
+    let secondNum = Math.floor( Math.random()*150 ) + 50;
+    let thirdNum = Math.floor( Math.random()*150 ) + 50;
+    let fourthNum = Math.floor( Math.random()*150 ) + 50;
+    let fifthNum = Math.floor( Math.random()*150 ) + 50;
+    let sixthNum = Math.floor( Math.random()*150 ) + 50;
+    let seventhNum = Math.floor( Math.random()*150 ) + 50;
+    let eighthNum = Math.floor( Math.random()*150 ) + 50;
+    shapes.forEach( shape =>
+        shape.style.borderRadius = `${firstNum}px ${secondNum}px ${thirdNum}px ${fourthNum}px/${fifthNum}px ${sixthNum}px ${seventhNum}px ${eighthNum}px`
+    );
+    
+}
+
+setInterval(changeShape, 2000);
